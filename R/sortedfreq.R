7b435bf1bc00868ddaddba0269c13918a00c3957
Paul Cantalupo <pcantalupo@gmail.com>
2018-06-25 21:38:14 -0400

    first commit

---
+++ R/sortedfreq.R
@@ -0,0 +1,15 @@
+#' Sorted frequency
+#'
+#' This function determines the frequency of a set of values. Returns the values and their frequencies. Values are sorted descending by frequency
+#' @param df The name of your dataframe
+#' @param colname The column name to calculate sorted frequency.
+#' @keywords sorted frequency
+#' @examples
+#' sortedfreq(mtcars, "carb")
+sortedfreq = function (df, colname) {
+  t = sort(table(df[,colname]), decreasing = T)
+  df = data.frame(x = names(t), freq = matrix(t))
+  colnames(df)[1] = colname
+  df
+}
+
